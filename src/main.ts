23bea0b9b67fb020f3c27931b1acf1c88a9fa8dd
Ethan Faccioli <ethanfaccioli@gmail.com>
2026-06-19 14:43:56 -0300

    chore: integracao manual com Firebase e Firestore via main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,28 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
+// Firebase
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+// Configuração do banco de dados
+const firebaseConfig = {
+  apiKey: "AIzaSyB5vvK4WDnJIT-TqfvS7x9Ee4rkmshRYkU",
+  authDomain: "vacinainfancia.firebaseapp.com",
+  projectId: "vacinainfancia",
+  storageBucket: "vacinainfancia.firebasestorage.app",
+  messagingSenderId: "206431395209",
+  appId: "1:206431395209:web:1b16140aa935640d6dd558"
+};
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+
+    // Ativando o Firebase e o Firestore
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore())
   ],
-});
+}).catch(err => console.error(err));
